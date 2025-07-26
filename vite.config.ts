2874170af1b4a779676f2cd57fb474b6464a6ac0
ishfaqmalik441 <ishfaqmalik441@gmail.com>
2025-07-26 15:37:12 +0800

    vite.config and package.json  updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ command }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: '/personal_website/',
+  base: command === 'serve' ? '/' : '/personal_website/',
   plugins: [
     react(),
   ].filter(Boolean),
