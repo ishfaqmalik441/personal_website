eb83c3e410c9ead955963cf435049a251513fbd2
ishfaqmalik441 <ishfaqmalik441@gmail.com>
2025-07-26 15:52:23 +0800

    vite-update

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ command }) => ({
     host: "::",
     port: 8080,
   },
-  base: command === 'serve' ? '/' : '/personal_website/',
+  base: '/',
   plugins: [
     react(),
   ].filter(Boolean),
